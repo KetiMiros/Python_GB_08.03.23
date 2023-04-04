3f4c5f573478d7fcecca6e1cdd49cd09f4bba245
Ketrin_Miros <miroslavskayaketi@gmail.com>
2023-04-05 00:42:25 +0300

    update les7_task2.py

--- lesson_7/les7_task2.py
+++ lesson_7/les7_task2.py
@@ -40,19 +40,18 @@
 #    6 |    6  12  18  24  30  36
 
 
-
-def table(operation, num_rows=6, num_columns=6 ):
+def table(operation, num_rows=6, num_colums=6):
     header = " "*7
-    for col in range(1, num_columns+1):
-        header +=f"{col:4d}"
-    print(header)
-    print(" "*7, "-"*num_columns*4)
-    for row in range(1, num_rows+1):
-        row_str = f"{row:4d} | "
+    for colums in range(1, num_colums + 1):
+        header += f"{colums:4d}"
+    print(f"{header} ")
+    for rows in range(1, num_rows + 1):
+        row_str = f"{rows:4d} | "
 
-        for col in range(1, num_columns+1):
-            row_str += f"{operation(row, col):4d}"
-
+        for colums in range(1, num_colums + 1):
+            row_str += f"{operation(rows, colums):4d}"
+        print("***"*13)
         print(row_str)
 
-table(lambda x,y: x*y)
+
+table(lambda x, y: x*y)
